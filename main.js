1db52ec5fab3ddea279549eb27b67e342bd9edfa
Renato Martinez Costa <renatomc@hotmail.com>
2021-04-19 21:25:09 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdate } = require('electron-updater');
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  mainWindow.once('ready-to-show', () => {
+    autoUpdate.checkForUpdatesAndNotify();
+  });
+}
+
+app.on('ready', function() {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', function(event) {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdate.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdate.on('updata-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
